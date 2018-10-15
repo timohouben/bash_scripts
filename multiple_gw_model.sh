0a7c95742b54e5f3d5b91caf5e65ff893d7f82f7
Timo Houben <timo.houben@ufz.de>
2018-10-15 16:10:56 +0200

    some changes and new scripts to start multiple stuffs

--- multiple_gw_model.sh
+++ multiple_gw_model.sh
@@ -5,11 +5,17 @@ dir_user=0
 
 
 # specify the STORATIVITIES for groundwater model deRooij 2012
-declare -a storages=(1E-5 1e-3)
-#declare -r thickness=30
+#declare -a storages=(0.0003 0.0006 0.0009 0.0012 0.0015 0.0018 0.0021 0.0024 0.0027 0.003 0.006 0.009 0.012 0.015 0.018 0.021 0.024 0.027 0.03 0.06 0.09 0.12 0.15 0.18 0.21 0.24 0.27 0.3)
+declare -a storages=(0.022201 0.022202)
+#declare -a names="1E-05 2E-05 3E-05 4E-05 5E-05 6E-05 7E-05 8E-05 9E-05 1E-04 2E-04 3E-04 4E-04 5E-04 6E-04 7E-04 8E-04 9E-04 1E-03 2E-03 3E-03 4E-03 5E-03 6E-03 7E-03 8E-03 9E-03 1E-02"
+declare -a names="1E-5 2E-5"
+names_arr=($names)
+
+counter=0
 
 for i in "${storages[@]}"
 do
+
 	#echo "${i}"
 	dir_new="${i}"
 	
@@ -26,17 +32,27 @@ do
 	rm Dupuitflow_neu.in
 	
 	
-	echo "GW Model de Rooij 2012 started. Folder name: "$dir_new
+	echo "GW Model de Rooij 2012 started. Folder name: "${names_arr[$counter]}
+	start_time="$(date -u +%s)"
 	./a
 	sleep 1
 	echo Done.
 	
-	mkdir $dir_new
-	echo "Plots and .OUT will be moved to folder" $dir_new "when you close the plot-window."
+	# calculate elapsed time and print
+	end_time="$(date -u +%s)"
+	elapsed="$(($end_time-$start_time))"
+	echo "Total of $elapsed seconds elapsed for calculation."
+	
+	
+	
+	mkdir ${names_arr[$counter]}
+	echo "Plots and .OUT will be moved to folder" ${names_arr[$counter]} "when you close the plot-window."
 	./plot.py
 	
-	mv *.OUT $dir_new/
-	cp *.in $dir_new/
-	mv *.png $dir_new/
+	mv *.OUT ${names_arr[$counter]}/
+	cp *.in ${names_arr[$counter]}/
+	mv *.png ${names_arr[$counter]}/
 	echo Plots and .OUT saved.
+	counter=$((counter + 1))
+	
 done
