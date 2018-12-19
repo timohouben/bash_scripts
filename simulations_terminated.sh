bd4fba548ec0434b244b0eeef99eda698809cd27
Timo Houben <timo.houben@ufz.de>
2018-12-19 10:48:01 +0100

    imporves simul_tem, it stores the foldernames whcih are not ready in a file and appends a date to compare with previos requests

--- simulations_terminated.sh
+++ simulations_terminated.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 declare -i count=0;
 ready=();
+not_ready=$(date);
+not_ready+="\n######################################################################"
 for i in *;
   do
     #line = $(wc -l < $i/*.OUT)
@@ -13,8 +15,11 @@ for i in *;
     elif tail -n 1 $i/*.OUT | grep -q "Executing ConstructGrid() ...";
     then
         echo $i is still constructing the grid;
+	not_ready+="\n $i is still constructing the grid"
     else
 	echo $i is not ready. Last time step: $(tail -n 100 $i/*.OUT | grep "Time step:");
+	not_ready+="\n $i Last time step: $(tail -n 100 $i/*.OUT | grep "Time step:")";
     fi;
   done
 echo $ready >> ready.txt
+echo -e $not_ready >> not_ready.txt
